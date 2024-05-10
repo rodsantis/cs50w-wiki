02c6a6394159b04736edfedfc1afd206b218a1c2
Rodrigo De Santis <Rodrigo@Rodrigos-MBP.homenet.telecomitalia.it>
2024-05-10 10:01:47 +0200

    Some comments were added before creating a new branch for the forms implementation

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from . import util
 
-# Using for testing
-from django.http import HttpResponse
-
-from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -29,3 +24,27 @@ def errorpage(request, name, number):
         "number": number,
         "name": name,
     })
+
+
+def wiki_search(request, name):
+    ...
+    # if request.method == "POST":
+    #     name = request.POST
+    #     name = name['q']
+    #     return HttpResponseRedirect(reverse("wiki", args=(q.id,)))
+    #     entry_list = util.list_entries()
+    #     if name in entry_list:
+    #         return render(request, "encyclopedia/wiki/<str:name>.html", {
+    #             "name": name,
+    #             "information": util.get_entry(name),
+    #         })
+    #     if name not in entry_list:
+    #         list_result = []
+    #         for item in entry_list:
+    #             if name in item:
+    #                 list_result.append(item)
+    #             continue
+    #         return render(request, "encyclopedia/wiki.html", {
+    #             "name": "Wiki",
+    #             "entries": list_result
+    #         })
